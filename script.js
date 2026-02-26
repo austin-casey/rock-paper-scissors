37c913a96a5de389cfa1b0f0ad0d5c9e6429d42c
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-25 16:32:51 -0900

    Updated rock-paper-scissors with DOM interaction

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 
-//***** Pseudocode
-// 1. Ask the user to choose rock, paper, or scissors
-// 2. Randomly choose a selection for the computer
-// 3. Evaluate the choices against each other to see who wins
-// 4. Keep track of the score over 5 rounds.  */
 
 const choiceOptions = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 0;
+const buttonDiv = document.querySelector('.buttons');
+const resultsDiv = document.querySelector('.results');
 
-// Gets the user's selection
-function getHumanChoice() {
-    const humanChoice = prompt('Choose rock, paper, or scissors.');
-    return humanChoice.toLowerCase();
-}
 // Gets the computer's selection
 function getComputerChoice() {
     const choiceNumber = Math.floor(Math.random() * 3);
@@ -24,29 +16,36 @@ function getComputerChoice() {
 
 // Play a round
 function playRound(humanChoice, computerChoice) {
+    rounds +=1;
     if (
         (humanChoice === 'rock' && computerChoice === 'scissors') || 
         (humanChoice === 'paper' && computerChoice === 'rock') || 
         (humanChoice === 'scissors' && computerChoice === 'paper')
     ) {
         humanScore += 1;
-        console.log(`You win! You chose ${humanChoice} and the computer chose ${computerChoice}.\nYou've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`)
+        resultsDiv.textContent = `You win! You chose ${humanChoice} and the computer chose ${computerChoice}.\nYou've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`
     } else if (humanChoice === computerChoice) {
-        console.log(`You tied. You both chose ${humanChoice}. You've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`)
+        resultsDiv.textContent = `You tied. You both chose ${humanChoice}. You've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`
     } else  {
         computerScore += 1;
-        console.log(`You lose. You chose ${humanChoice} and the computer chose ${computerChoice}.\nYou've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`)
+        resultsDiv.textContent = `You lose. You chose ${humanChoice} and the computer chose ${computerChoice}.\nYou've completed ${rounds} round(s) and the score is: \nUser: ${humanScore}\nComputer: ${computerScore}`
+    }
+
+    if (humanScore === 5 || computerScore === 5){
+        let winner = "";
+        if (humanScore === 5) {
+            winner = "Human";
+        } else {
+            winner = "Computer"
+        }
+        
+        resultsDiv.append(` Game over. The winner is ${winner}!`)
+        buttonDiv.style.display = "none";
     }
 }
 
-// Play a game 
-function playGame() {
-    while (rounds < 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        rounds += 1;
-        playRound(humanSelection, computerSelection);
-    }
-}
-
-playGame();
+buttonDiv.addEventListener("click", (e) => {
+    const humanChoice = e.target.id;
+    const computerChoice = getComputerChoice(); 
+    playRound(humanChoice, computerChoice);
+})
